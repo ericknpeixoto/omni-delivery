46e747293f2e44655df8278f8cc6f3a08901ec5f
João Victor <joaov_rc@outlook.com>
2021-11-02 23:58:44 -0300

    UberEats com MongoDB

--- UberEats/server.js
+++ UberEats/server.js
@@ -1,102 +1,110 @@
-const express = require ('express')
-const app = express()
-app.use(express.json())
+const express = require ('express');
+const app = express();
+var mongoose = require("mongoose");
+var Pedido = require("./app/models/pedido");
+mongoose.connect("mongodb://localhost:27017/UberEats");
 
+app.use(express.json());
+var router = express.Router();
 
-let contador = 3
-let pedidos = [
-    {
+router.use(function (req, res, next){
+    console.log("Algo está acontecendo aqui...");
+    next();
+});
+
+router.route("/pedido")
+    /* 1) Método: Criar pedido (acessar em: POST http://localhost:4000/pedido)  */
+    .post(function(req, res) {
+        var pedido = new Pedido();
+        pedido.cnpjRestaturante = req.body.cnpjRestaturante;
+        pedido.cpfCliente = req.body.cpfCliente;
+        pedido.valor = req.body.valor;
+        pedido.cupomDesconto = req.body.cupomDesconto;
+        pedido.total = req.body.total;
+        pedido.enderecoEntrega = req.body.enderecoEntrega;
+        pedido.cidade = req.body.cidade;
+        pedido.estado = req.body.estado;
+        pedido.uf = req.body.uf;
+        pedido.cep = req.body.cep;
+        pedido.dataSolicitacao = req.body.dataSolicitacao;
+        pedido.prazoEntrega = req.body.prazoEntrega;
+        pedido.dataFinalizacao = req.body.dataFinalizacao;
+        pedido.situacao = req.body.situacao;
         
-        idPedido: 1,
-        cnpjRestaturante: '11.939.409/0001-94',
-        cpfCliente: '515.539.680-85',
-        valor: 90.00,
-        cupomDesconto: 15.00,
-        total: 75.00,
-        enderecoEntrega: 'Rua das Ruas, 14',
-        cidade: 'São Paulo',
-        estado: 'São Paulo',
-        uf: 'SP',
-        cep: '03928232',
-        dataSolicitacao: '2021-10-15T08:05:00Z',
-        prazoEntrega: '2021-10-15T09:00:00Z',
-        dataFinalizacao: '2021-10-15T09:05:00Z',
-        situacao: 'Entregue',       
-    },
-    {
-        idPedido: 2,
-        cnpjRestaturante: '39.169.871/0001-79',
-        cpfCliente: '493.591.420-34',
-        valor: 50.00,
-        cupomDesconto: 5.00,
-        total: 45.00,
-        enderecoEntrega: 'Rua das Ruas, 14',
-        cidade: 'São Paulo',
-        estado: 'São Paulo',
-        uf: 'SP',
-        cep: '03928232',
-        dataSolicitacao: '2021-10-16T19:15:00Z',
-        prazoEntrega: '2021-10-16T20:10:00Z',
-        dataFinalizacao: '2021-10-16T20:05:00Z',
-        situacao: 'Entregue',
-    }
-]
+        pedido.save(function(error) {
+            if(error)
+                res.send('Erro ao tentar salvar o Pedido....: ' + error);
+            
+            res.status(201).json({ message: 'Pedido Cadastrado com Sucesso!' });
+        });
+    })
 
-app.post('/pedido', (req, res) => {
-    let pedido = preencherObjetoPedido(contador++, req.body)
-    pedidos.push(pedido)
-    res.status(201).json(pedidos)
-})
+    /* 2) Método: Selecionar todos pedidos (acessar em: GET http://localhost:4000/pedido)  */
+    .get(function(req, res) {
+        Pedido.find(function(error, pedidos) {
+            if(error) 
+                res.send('Erro ao tentar Selecionar Todos os pedidos...: ' + error);
 
-app.get('/pedido', (req, res) => {
-    res.json(pedidos)
-})
+            res.json(pedidos);
+        });
+    });
 
+//Rotas que irão terminar em '/pedido/:pedido_id':
+router.route('/pedido/:pedido_id')
 
-app.put('/pedido/:idPedido', (req, res) => {
-    let idPedido = req.params.idPedido
-    let indice = pedidos.findIndex(pedido => pedido.idPedido == idPedido)
-    let pedido = preencherObjetoPedido(idPedido, req.body)
+    /* 3) Método: Selecionar por Id: (acessar em: GET http://localhost:4000/pedido/:pedido_id) */
+    .get(function (req, res) {
+        Pedido.findById(req.params.pedido_id, function(error, pedido) {
+            if(error)
+                res.send('Id do Pedido não encontrado....: ' + error);
 
-    pedidos[indice] = pedido
+            res.json(pedido);
+        });
+    })
 
-    res.status(200).json(pedidos)
-})
+    /* 4) Método: Atualizar por Id: (acessar em: PUT http://localhost:4000/pedido/:pedido_id) */
+    .put(function(req, res) {
+        Pedido.findById(req.params.pedido_id, function(error, pedido) {
+            if (error) res.send("Id do Pedido não encontrado....: " + error);
 
-app.delete('/pedido/:idPedido', (req, res) => {
-    let idPedido = req.params.idPedido
-    let indice = pedidos.findIndex(pedido => pedido.idPedido == idPedido)
-    if (indice == -1) {
-        res.status(204)
-    }
+            pedido.cnpjRestaturante = req.body.cnpjRestaturante;
+            pedido.cpfCliente = req.body.cpfCliente;
+            pedido.valor = req.body.valor;
+            pedido.cupomDesconto = req.body.cupomDesconto;
+            pedido.total = req.body.total;
+            pedido.enderecoEntrega = req.body.enderecoEntrega;
+            pedido.cidade = req.body.cidade;
+            pedido.estado = req.body.estado;
+            pedido.uf = req.body.uf;
+            pedido.cep = req.body.cep;
+            pedido.dataSolicitacao = req.body.dataSolicitacao;
+            pedido.prazoEntrega = req.body.prazoEntrega;
+            pedido.dataFinalizacao = req.body.dataFinalizacao;
+            pedido.situacao = req.body.situacao;
 
-    pedidos.splice(indice, 1)
-    res.status(200).json(pedidos)
-})
+            pedido.save(function(error) {
+                if(error)
+                    res.send('Erro ao atualizar o pedido....: ' + error);
 
-function preencherObjetoPedido(idPedido, body){
-    return {
-        idPedido: idPedido,
-        cnpjRestaturante: body.cnpjRestaturante,
-        cpfCliente: body.cpfCliente,
-        custo: body.custo,
-        desconto: body.desconto,
-        total: body.total,
-        dataInicio: body.dataInicio,
-        dataEntrega: body.dataEntrega,
-        dataFim: body.dataFim,
-        status: body.status,
-        logradouro: body.logradouro,
-        numero: body.numero,
-        complemento: body.complemento,
-        cidade: body.cidade,
-        estado: body.estado,
-        uf: body.uf,
-        cep: body.cep
-    }
-} 
+                res.status(200).json({ message: 'Pedido atualizado com sucesso!' });
+            });
+        });
+    })
+
+    /* 5) Método: Excluir por Id (acessar: http://localhost:4000/pedido/:pedido_id) */
+    .delete(function(req, res) {
+        
+        Pedido.remove({
+            _id: req.params.pedido_id
+            }, function(error) {
+                if (error) res.send("Id do Pedido não encontrado....: " + error);
+
+                res.json({ message: 'Pedido Excluído com Sucesso!' });
+            });
+    });
 
 
+app.use(router);
 
-app.listen(3000, () => console.log ("Classificação. Porta 3000."))
+app.listen(4000, () => console.log ("UberEats. Porta 4000."))
 
